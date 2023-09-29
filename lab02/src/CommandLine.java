12c4ad6d2f4c4d37ba747c8d9d99127534830bd7
Desuche <uchedestinyx@gmail.com>
2023-09-29 11:48:52 +0800

    Refactored program structure; Added intro, help, and extro;

--- lab02/src/CommandLine.java
+++ lab02/src/CommandLine.java
@@ -5,11 +5,95 @@ import java.util.Scanner;
 
 public class CommandLine {
 
-    private String currentPath = new File("").getCanonicalPath();
+    private String currentPath;
+    private final Scanner scanner;
 
     public CommandLine() throws IOException {
+        this.currentPath = new File("").getCanonicalPath();
+        this.scanner = new Scanner(System.in);
     }
 
+    public void run() throws IOException {
+        printIntro();
+        while (true){
+            String[] input = scanner.nextLine().split("\\s+(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            processInput(input);
+        }
+    }
+
+    private void printIntro(){
+        System.out.println("Command Line Emulator");
+        System.out.println("Type 'help' for a list of available commands.");
+        System.out.println();
+        System.out.print(currentPath + "> ");
+    }
+
+    private void processInput(String[] input) throws IOException{
+        if (input[0].isEmpty()){
+            System.out.print(currentPath + "> ");
+            return;
+        }
+
+        try {
+
+            switch (input[0]) {
+                case "dir":
+                    dir(resolvePath(input[1]));
+                    break;
+                case "md":
+                    md(resolvePath(input[1]));
+                    break;
+                case "rd":
+                    rd(resolvePath(input[1]));
+                    break;
+                case "del":
+                    del(resolvePath(input[1]));
+                    break;
+                case "cd":
+                    cd(resolvePath(input[1]));
+                    break;
+                case "help":
+                    help();
+                    break;
+                case "exit":
+                    exit();
+                    break;
+                default:
+                    System.out.printf("%s is not a recognised command\n", input[0]);
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Your input is incomplete");
+        }
+
+        System.out.print(currentPath + "> ");
+    }
+
+    private String resolvePath(String pathname) throws IOException {
+        String resolvedPath;
+        int stringLength = pathname.length();
+
+        if (stringLength > 0 && pathname.charAt(0) == '"'){
+
+            if (pathname.charAt(stringLength - 1) == '"'){
+                pathname = pathname.substring(1, stringLength - 1);
+
+            } else {
+                pathname = pathname.substring(1, stringLength);
+            }
+
+        }
+
+        if (stringLength > 0 && pathname.charAt(0) == '~'){
+            pathname = pathname.substring(1, stringLength);
+            String root = Paths.get(currentPath).getRoot().toString();
+            resolvedPath = ( new File(Paths.get(root, pathname).toAbsolutePath().toString()) ).getCanonicalPath();
+            return resolvedPath;
+        }
+
+        resolvedPath = ( new File(Paths.get(currentPath, pathname).toAbsolutePath().toString()) ).getCanonicalPath();
+        return resolvedPath;
+    }
 
     private void dir(String pathname){
         File path = new File(pathname);
@@ -76,83 +160,39 @@ public class CommandLine {
         this.currentPath = pathname;
     }
 
+    private void help(){
+        String[] messages = {
+                "Hello there ~",
+                "Here are the available commands",
+                "    dir <path> : list out a directory's contents",
+                "    md <path to new directory> : make a single or multilevel directory",
+                "    cd <path> : Switch to a new directory",
+                "    del <path to file> : delete a FILE",
+                "    rd <path to empty directory> : remove an EMPTY DIRECTORY",
+                "    exit : quit this program",
+                "",
+                "When working with paths:",
+                "- relative paths are accepted",
+                "- use '~' at the start of a path to signify the root directory",
+                "- if your paths contain spaces, wrap them in double quotation marks"
+        };
+
+        System.out.println();
+        for (String m : messages){
+            System.out.println(m);
+        }
+        System.out.println();
+    }
+
     private void exit(){
+        System.out.println();
+        System.out.println("Thank you for using this tool");
         System.exit(0);
-    }
-
-    private void processInput(String[] input) throws IOException{
-        if (input[0].isEmpty()){
-            System.out.print(currentPath + "> ");
-            return;
-        }
-
-        try {
-
-            switch (input[0]) {
-                case "dir":
-                    dir(resolvePath(input[1]));
-                    break;
-                case "md":
-                    md(resolvePath(input[1]));
-                    break;
-                case "rd":
-                    rd(resolvePath(input[1]));
-                    break;
-                case "del":
-                    del(resolvePath(input[1]));
-                    break;
-                case "cd":
-                    cd(resolvePath(input[1]));
-                    break;
-                case "exit":
-                    exit();
-                    break;
-                default:
-                    System.out.printf("%s is not a recognised command\n", input[0]);
-            }
-
-        } catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Your input is incomplete");
-        }
-
-        System.out.print(currentPath + "> ");
-    }
-
-    private String resolvePath(String pathname) throws IOException {
-        String resolvedPath;
-        int stringLength = pathname.length();
-
-        if (stringLength > 0 && pathname.charAt(0) == '"'){
-
-            if (pathname.charAt(stringLength - 1) == '"'){
-                pathname = pathname.substring(1, stringLength - 1);
-
-            } else {
-                pathname = pathname.substring(1, stringLength);
-            }
-
-        }
-
-        if (stringLength > 0 && pathname.charAt(0) == '~'){
-            pathname = pathname.substring(1, stringLength);
-            String root = Paths.get(currentPath).getRoot().toString();
-            resolvedPath = ( new File(Paths.get(root, pathname).toAbsolutePath().toString()) ).getCanonicalPath();
-            return resolvedPath;
-        }
-
-        System.out.println(pathname);
-        resolvedPath = ( new File(Paths.get(currentPath, pathname).toAbsolutePath().toString()) ).getCanonicalPath();
-        return resolvedPath;
     }
 
     public static void main(String[] args) throws IOException {
         CommandLine commandLine = new CommandLine();
-        Scanner scanner = new Scanner(System.in);
+        commandLine.run();
 
-        System.out.print(commandLine.currentPath + "> ");
-        while (true){
-            String[] input = scanner.nextLine().split("\\s+(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-            commandLine.processInput(input);
-        }
     }
 }
